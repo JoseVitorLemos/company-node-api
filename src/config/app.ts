5867b184ff6499920dfd343af1ece5a55e8e7fd6
jose vitor <jose81073175@gmail.com>
2022-01-21 22:01:30 -0300

    feat: set request body using json

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import routes from '../route'
+import bodyParser from '../infra/middleware/body-parser'
 
 const app = express()
+
+app.use(bodyParser)
 
 app.use(routes)
 
