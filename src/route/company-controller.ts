77df3bb53cfb6bcd10f07203a905707fe33e3288
jose vitor <jose81073175@gmail.com>
2022-01-22 01:07:08 -0300

    feat: create method post company

--- src/route/company-controller.ts
+++ src/route/company-controller.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express'
+import knex from '../infra/database/connection'
 
 const companyController = Router()
 
-companyController.get('/', (req, res) => {
-	res.send('Ok!')
+companyController.post('/', async (req, res) => {
+	const { uf, trade_name, cnpj } = req.body
+
+	try {
+		const id = await knex('companys')
+			.insert({
+				uf,
+				trade_name,
+				cnpj,
+				created_at: new Date()
+			}).returning('id').then(id => id[0])
+
+		const company = await knex('companys').where('id', id).first()
+
+		res.status(201).json(company)
+
+	} catch (err) {
+		console.log(err)
+		res.status(500).send({ statusCode: 500, 	message: 'Create company fail' })
+	}
 })
 
 export default companyController
