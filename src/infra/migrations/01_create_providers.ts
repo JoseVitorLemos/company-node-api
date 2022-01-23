332bda6b36c0a24f5d1e43286c7a1c6db0cfd445
jose vitor <jose81073175@gmail.com>
2022-01-23 16:03:56 -0300

    refactor: change collumn to nullable

--- src/infra/migrations/01_create_providers.ts
+++ src/infra/migrations/01_create_providers.ts
@@ -4,8 +4,8 @@ export async function up(knex: Knex) {
 	return knex.schema.createTable('providers', table => {
 		table.increments('id').primary()
 		table.string('name').notNullable()
-		table.string('cpf').notNullable()
-		table.string('cnpj').notNullable()
+		table.string('cpf').nullable()
+		table.string('cnpj').nullable()
 		table.string('phone').notNullable()
 		table.date('birth_date').nullable()
 		table.dateTime('created_at').notNullable()
